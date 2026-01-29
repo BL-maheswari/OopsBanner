346d109f41e236b7e28e5348c21b3eb82e642e9c
BL-maheswari <ipperu.maheswari@bridgelabz.com>
2026-01-29 14:58:40 +0530

    feature(UC1) : Print OOPS to Console

---
+++ com.blz.oopsbanner/OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ * @author Developer
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
